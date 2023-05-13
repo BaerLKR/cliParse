1ca0e4ceee3460f29668f9e22af765aaed447c4f
Lovis Rentsch <gpg@lovirent.eu>
2023-05-13 15:40:06 +0200

    base system

--- src/arg.rs
+++ src/arg.rs
@@ -1,9 +1,48 @@
+#[derive(PartialEq)]
 pub enum Argument {
+    // change what arguments are possible here!
+    None,
     Help,
     Test,
     Demo,
 }
 pub fn collect() -> Vec<Argument>{
-
-   vec![Argument::Help] 
+    let arg: Vec<String> = std::env::args().collect();
+    let arg = if arg.len() > 1 {
+        Some(arg)
+    } else {
+        None
+    };
+    let re: Vec<Argument>;
+    match arg {
+        Some(v) => {
+            re = eval(v);
+        },
+        None => {
+            re = vec![Argument::None]
+        }
+    }
+    return re;
+}
+fn eval(data: Vec<String>) -> Vec<Argument> {
+    let mut re: Vec<Argument> = Vec::new();
+    for n in data {
+        match n.as_str() {
+            //chang how the arguments can be reached here!
+            "help" | "-h" | "--help" => {
+                re.push(Argument::Help);
+            },
+            "test" => {
+                re.push(Argument::Test);
+            }
+            _ => {}
+        }
+    }
+    //add rules here
+    //here I only count the "Help" argument when it is called once
+    if re.contains(&Argument::Help) {
+        re.clear();
+        re.push(Argument::Help);
+    }
+    re
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use colored::*;
 pub mod arg;
 fn main() {
-    let cmd: Vec<Argument> = arg::collect();
+    let cmd: Vec<arg::Argument> = arg::collect();
+    for n in cmd {
+        match n {
+            arg::Argument::Help => println!("{}", "Help".yellow()),
+            arg::Argument::Test => println!("Test"),
+            _ => {}
+        }
+    }
 }
